2781e10dd9207971d329bfa71178bc7477c4c26c
Igor R. Guariroba <65466371+IgorGuariroba@users.noreply.github.com>
2024-05-01 04:40:43 +0000

    Guardar informações ulteis do projeto

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./*.html", // Todos os arquivos HTML na raiz do projeto
+    "./styles.css", // Seu arquivo CSS
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
